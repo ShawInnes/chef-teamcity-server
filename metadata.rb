3b7010de50fd8e5f1ec128838bb71ca421df6f9d
dueckes <matthew@ueckerman.net>
2013-06-19 11:58:34 +1000

    Downgraded to version 0.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "matthew.ueckerman@myob.com"
 license          "All rights reserved"
 description      "Installs/Configures a TeamCity Server for production use"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.0.0"
 
 depends "java", "~> 1.11.4"
 depends "teamcity_server", "~> 0.0.0"
