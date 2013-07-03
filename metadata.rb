68457ee3889bb43bd2c8337fd2be1517b7cd66b2
dueckes <matthew@ueckerman.net>
2013-07-03 15:46:33 +1000

    + recipe description

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,15 @@
-name             "teamcity-server"
-maintainer       "Matthew Ueckerman"
-maintainer_email "matthew.ueckerman@myob.com"
-license          "All rights reserved"
-description      "Installs/Configures a TeamCity Server for production use"
+name             'teamcity-server'
+maintainer       'Matthew Ueckerman'
+maintainer_email 'matthew.ueckerman@myob.com'
+license          'All rights reserved'
+description      'Installs/Configures a TeamCity Server for production use'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.4"
+version          '0.0.4'
 
-depends "java", "~> 1.11.4"
-depends "git", "~> 2.5.2"
-depends "postgresql", "~> 0.10.1"
+recipe 'teamcity-server', 'Installs/Configures a TeamCity Server for production use'
 
-supports "ubuntu", "= 12.04"
+depends 'java', '~> 1.11.4'
+depends 'git', '~> 2.5.2'
+depends 'postgresql', '~> 0.10.1'
+
+supports 'ubuntu', '= 12.04'
