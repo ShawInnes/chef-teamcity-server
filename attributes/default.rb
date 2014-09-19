88c8e6c3a933a9cfcc2f6dc2f822cfea6665e955
Daryl Robbins <daryl@robbins.name>
2014-09-19 09:43:48 -0400

    Must export TEAMCITY_DATA_PATH

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,6 @@ default["teamcity-server"]["address"] = "0.0.0.0"
 default["teamcity-server"]["port"] = 80
 default["teamcity-server"]["postgresql"]["version"] = "9.2"
 default["teamcity-server"]["postgresql"]["driver_version"] = "9.2-1002"
-default["teamcity-server"]["postgresql"]["locale"] = "en_AU.UTF-8"
+default["teamcity-server"]["postgresql"]["locale"] = "en_US.UTF-8"
 default["teamcity-server"]["git"]["version"] = "1.8.3"
 default["teamcity-server"]["data_directory"] = "/var/lib/teamcity"
